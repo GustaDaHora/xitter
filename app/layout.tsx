8c702d0ec54d80176ffac077bd0df83c277a36e0
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2026-01-08 01:21:18 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Providers from "@/components/providers";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggle } from "@/components/theme-toggle";
 import MainLayout from "@/components/layout/main-layout";
 
 export const metadata: Metadata = {
@@ -29,7 +31,20 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-          <MainLayout>{children}</MainLayout>
+          <ThemeProvider
+            attribute="data-theme"
+            defaultTheme="dark"
+            enableSystem={false}
+            themes={[
+              "light",
+              "light-high-contrast",
+              "dark",
+              "dark-high-contrast",
+            ]}
+          >
+            <MainLayout>{children}</MainLayout>
+            <ThemeToggle />
+          </ThemeProvider>
         </Providers>
       </body>
     </html>
